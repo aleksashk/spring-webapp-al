d309f1c46ba48fb8f5a2cc04cc16ae48c4f9baf6
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-06 23:14:44 +0300

    configuration by annotation

--- src/main/java/ru/philimonov/springcourse/MusicPlayer.java
+++ src/main/java/ru/philimonov/springcourse/MusicPlayer.java
@@ -1,28 +1,10 @@
 package ru.philimonov.springcourse;
 
 public class MusicPlayer {
-    private String name;
-
-    private int volume;
-
     private Music music;
 
-    public MusicPlayer() {}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
+    public MusicPlayer(Music music) {
+        this.music = music;
     }
 
     public void setMusic(Music music) {
--- src/main/java/ru/philimonov/springcourse/SpringRunner.java
+++ src/main/java/ru/philimonov/springcourse/SpringRunner.java
@@ -6,8 +6,12 @@ public class SpringRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("rockMusicBean", Music.class);
-        System.out.println(music.getSong());
+        Music music1 = context.getBean("rockMusicBean", Music.class);
+        Music music2 = context.getBean("classicalMusicBean", Music.class);
+        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
+        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+        musicPlayer1.playMusic();
+        musicPlayer2.playMusic();
 
         context.close();
     }
