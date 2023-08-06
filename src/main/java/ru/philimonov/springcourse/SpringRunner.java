a683fe859dd30b8c6bb1e9479980885cc3bacd0e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-06 22:45:30 +0300

    scope singleton

--- src/main/java/ru/philimonov/springcourse/SpringRunner.java
+++ src/main/java/ru/philimonov/springcourse/SpringRunner.java
@@ -6,10 +6,12 @@ public class SpringRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 
-        System.out.println(musicPlayer.getName() + " " + musicPlayer.getVolume());
+        System.out.println("musicPlayer1 == musicPlayer2: " + (musicPlayer1 == musicPlayer2));
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
 
         context.close();
     }
