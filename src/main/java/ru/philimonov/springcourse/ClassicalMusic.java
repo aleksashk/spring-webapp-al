6d3f15f63a97753194b339802165334401888c7f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-06 22:55:45 +0300

    bean lifecycle init and destroy methods

--- src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
@@ -1,6 +1,15 @@
 package ru.philimonov.springcourse;
 
 public class ClassicalMusic implements Music {
+
+    public void init(){
+        System.out.println("DOING initialization");
+    }
+
+    public void destroy(){
+        System.out.println("DOING destroy");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/ru/philimonov/springcourse/SpringRunner.java
+++ src/main/java/ru/philimonov/springcourse/SpringRunner.java
@@ -6,17 +6,8 @@ public class SpringRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
-
-        musicPlayer1.setVolume(12);
-
-        System.out.println("musicPlayer1 == musicPlayer2: " + (musicPlayer1 == musicPlayer2));
-
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 
         context.close();
     }
