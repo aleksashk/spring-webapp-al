4311759a11231538f43b8a217d02ae7621672e9c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-06 22:58:42 +0300

    bean lifecycle init and destroy methods for prototype scope

--- src/main/java/ru/philimonov/springcourse/SpringRunner.java
+++ src/main/java/ru/philimonov/springcourse/SpringRunner.java
@@ -6,8 +6,10 @@ public class SpringRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong());
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic1.getSong());
+        System.out.println(classicalMusic2.getSong());
 
         context.close();
     }
