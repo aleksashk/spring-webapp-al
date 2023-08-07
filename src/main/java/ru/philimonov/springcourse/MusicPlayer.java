f567a3e221de1fe4a938d091ce9ce5a57520d2a3
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-07 07:55:15 +0300

    DI and @Autowired @Qualifier

--- src/main/java/ru/philimonov/springcourse/MusicPlayer.java
+++ src/main/java/ru/philimonov/springcourse/MusicPlayer.java
@@ -1,22 +1,18 @@
 package ru.philimonov.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+    @Qualifier("classicalMusicBean")
+    private Music music;
 
     public String playMusic() {
-        return "Playing: " + rockMusic.getSong();
+        return "Playing: " + music.getSong();
     }
 
 }
--- src/main/java/ru/philimonov/springcourse/SpringRunner.java
+++ src/main/java/ru/philimonov/springcourse/SpringRunner.java
@@ -6,8 +6,8 @@ public class SpringRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.playMusic());
 
         context.close();
     }
